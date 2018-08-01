b746758521f3751113803c7ef875e754f65a3bde
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-01 17:47:02 +0530

    new

--- compare_AB.py
+++ compare_AB.py
@@ -1,11 +1,11 @@
-varA = int(input("enter num  "))
-varB = int(input("enter another num  "))
-if varA is str and varB is str:
-	print ("String involved")
-if varA > varB:
-	print ("bigger")
-if varA == varB:
-	print ("equal")
-if varA < varB:
-	print ("lesser")
-
+varA = input("enter num  ")
+varB = input("enter another num  ")
+if (varA.isnumeric() and varB.isnumeric()):
+	if(varA>varB):
+	 	print("bigger")
+	elif(varA==varB):
+		print("equal")
+	else:
+		print("lesser")
+else:
+	print("String involved")
